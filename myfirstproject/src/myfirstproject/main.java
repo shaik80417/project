e79020517f37766e9123cd6a5733f1b44a17eb87
shaiksha <shaiksha80417@gmail.com>
2024-08-04 17:12:48 +0530

    first commit

---
+++ myfirstproject/src/myfirstproject/main.java
@@ -0,0 +1,52 @@
+
+package myfirstproject;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		//simple project
+	    // System.out.println("hello world");
+		//System.out.println("i love java");
+		//System.out.println("java is best");
+		
+		   //initializing
+		//int x=123;
+		//System.out.println("My number is:"+x);
+		
+		
+		//String x= "water";
+		//String y= "juice";
+		//String z;
+		//x=y;
+		//y=x;
+		
+		//z=x;
+		//x=y;
+		//y=z;
+		
+		//System.out.println("x:" +x);
+		//System.out.println("y:" +y);
+		
+		//adding values - variables 
+		//Scanner scanner = new Scanner(System.in);
+		
+		//System.out.println("what is you name?");
+		//String name = scanner.nextLine();
+		//System.out.println("How old are you");
+		//int age = scanner.nextInt();
+		//scanner.nextLine();
+		//System.out.println("what is your favorite food");
+		//String food = scanner.nextLine();
+		//System.out.println("hello " +name);
+		//System.out.println("you are "+age+"years old ");
+		//System.out.println("you like " +food);
+		
+		
+	// about expressions
+		
+	}
+	
+}
+	
